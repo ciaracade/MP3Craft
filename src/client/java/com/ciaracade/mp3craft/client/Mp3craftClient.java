5e008ff691cfbebe21b5be9d633590ff7f04266e
cadeci <ciaracade03@gmail.com>
2024-09-20 23:49:05 -0400

    chore: replace all files

---
+++ src/client/java/com/ciaracade/mp3craft/client/Mp3craftClient.java
@@ -0,0 +1,10 @@
+package com.ciaracade.mp3craft.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Mp3craftClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/ciaracade/mp3craft/Mp3craft.java
@@ -0,0 +1,10 @@
+package com.ciaracade.mp3craft;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Mp3craft implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
